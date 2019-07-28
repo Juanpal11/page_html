52cc7abbe66b4bd6d8c706dd90e7725a805b1669
Juan Bedoya <juan_pal19@hotmail.com>
2019-07-28 17:28:13 -0500

    con slides sin json

--- js/images.js
+++ js/images.js
@@ -2,9 +2,9 @@ var i = 0;
 var images = [];
 var time = 3000;
 
-images[0]= "image-big.png"
-images[1]= "image-big2.png"
-images[2]= "image-big3.png"
+images[0]= "assets/image-big.png"
+images[1]= "assets/image-big2.png"
+images[2]= "assets/image-big3.jpg"
 
 function changeImg(){
     document.slide.src = images[i];
@@ -17,5 +17,3 @@ function changeImg(){
 
 }
 window.onload = changeImg;
-// var mydata = JSON.parse(data);
-// document.write([0])
